4e40bda223930d2edc320a31fd26b1254a9793bf
Spirou07 <lefitjunior@hotmail.com>
2024-09-05 19:12:22 +0200

    Levels v2 + level reward

--- app/models/level.py
+++ app/models/level.py
@@ -1,32 +1,32 @@
 from bson import ObjectId
-from typing import Dict, Optional
+from typing import Dict, Optional, List
+
+from .reward import Reward
 from ..utils.strUtils import str_to_slug
 
 
 class Level:
-  def __init__(self, name: str, floor: int, number: int, cost: int, _id: Optional[str] = None):
+  def __init__(self, name: str, cost: int, rewards: List[Reward], _id: Optional[str] = None):
     self._id = ObjectId(_id) if _id else None
     self.name = name
-    self.floor = floor
-    self.number = number
     self.cost = cost
+    self.rewards = rewards
 
   @classmethod
   def from_dict(cls, data: Dict):
     return cls(
       _id = str(data.get('_id')) if data.get('_id') else None,
       name = str_to_slug(data.get('name')),
-      floor = data.get('floor'),
-      number = data.get('number'),
-      cost= data.get('cost')
+      cost= data.get('cost'),
+      rewards = [Reward.from_dict(reward_data) for reward_data in data.get('rewards', []) if
+               isinstance(reward_data, dict)]
     )
 
   def to_dict(self) -> Dict:
     level = {
       "name": self.name,
-      "floor": self.floor,
-      "number": self.number,
-      "cost": self.cost
+      "cost": self.cost,
+      'rewards': [reward.to_dict() for reward in self.rewards] if self.rewards else [],
     }
     if self._id:
       level["_id"] = str(self._id)
@@ -34,29 +34,45 @@ class Level:
     return level
 
   def create(self, db):
-    if not self._id:
-      result = self.read_by_level(db, self.name, self.floor, self.number)
-      if result:
-        return result
-      result = db.levels.insert_one(self.to_dict())
-      self._id = result.inserted_id
-    else:
-      db.levels.update_one({"_id": self._id}, {"$set": self.to_dict()})
+    existing = self.read_by_level(db, self.name)
+    if existing:
+      return existing
+    result = db.levels.insert_one(self.to_dict())
+    self._id = result.inserted_id
     return self
 
-  @staticmethod
-  def read_by_id(db, level_id):
-    data = db.levels.find_one({"_id": ObjectId(level_id)})
-    return Level.from_dict(data) if data else None
-  
+  def add_reward(self, db, reward_data):
+    existing_rewards = [existing_reward for existing_reward in self.rewards if existing_reward.quantity == reward_data.quantity]
+    if len(existing_rewards) == 1:
+      reward = existing_rewards[0]
+      reward.appearances += 1
+    elif len(existing_rewards) == 0:
+      reward = Reward(reward_data.quantity, 1)
+      self.rewards.append(reward)
+    else:
+      return None
+    db.levels.update_one({"_id": self._id}, {"$set": {'rewards': [reward.to_dict() for reward in self.rewards]}})
+    return self
+
+  def expected_reward(self):
+    if len(self.rewards) == 0:
+      return 0
+    quantities = [reward.quantity for reward in self.rewards]
+    return sum(quantities) / len(quantities)
+
   @staticmethod
   def read_by_name(db, level_name):
     data = db.levels.find_one({"name_slug": str_to_slug(level_name)})
     return Level.from_dict(data) if data else None
 
   @staticmethod
-  def read_by_level(db, level_name, level_floor, level_number):
-    data = db.levels.find_one({"name": str_to_slug(level_name), "floor": level_floor, "number": level_number})
+  def read_by_level2(db, level_name):
+    data = db.levels.find_one({"name": str_to_slug(level_name)})
+    return Level.from_dict(data) if data else None
+
+  @staticmethod
+  def read_by_level(db, level_name):
+    data = db.levels.find_one({"name": str_to_slug(level_name)})
     return Level.from_dict(data) if data else None
 
   @staticmethod
--- app/views/level.py
+++ app/views/level.py
@@ -3,6 +3,13 @@ from app.services.level import LevelService
 
 levels_blueprint = Blueprint('levels', __name__)
 
+@levels_blueprint.route('/levels', methods=['GET'])
+def get_levels():
+  levels = LevelService.get_all_levels()
+  if levels:
+    return jsonify([level.to_dict() for level in levels])
+  return jsonify({'error': 'Levels not found'}), 404
+
 @levels_blueprint.route('/levels', methods=['POST'])
 def add_level():
   level_data = request.json
@@ -11,22 +18,23 @@ def add_level():
 
 @levels_blueprint.route('/levels/<level>', methods=['GET'])
 def get_level_by_id(level):
-  level_obj = LevelService.get_one_level_by_id(level)
-  if level_obj:
-    return jsonify(level_obj.to_dict())
-  return jsonify({'error': 'Level not found'}), 404
-
-@levels_blueprint.route('/levels/<level>/floor/<floor>/number/<number>', methods=['GET'])
-def get_level(level, floor, number):
-  level_obj = LevelService.get_one_level(str(level), int(floor), int(number))
+  level_obj = LevelService.get_one_level(level)
   if level_obj:
     return jsonify(level_obj.to_dict())
   return jsonify({'error': 'Level not found'}), 404
 
 
-@levels_blueprint.route('/levels', methods=['GET'])
-def get_levels():
-  levels = LevelService.get_all_levels()
-  if levels:
-    return jsonify([level.to_dict() for level in levels])
-  return jsonify({'error': 'Levels not found'}), 404
+@levels_blueprint.route('/levels/<level>/reward', methods=['POST'])
+def add_reward(level):
+  reward_data = request.json
+  level = LevelService.add_reward(level, reward_data)
+  if level:
+    return jsonify(level.to_dict())
+  return jsonify({'error': 'Level not found'}), 404
+
+@levels_blueprint.route('/levels/<level>/reward', methods=['GET'])
+def get_expected_reward(level):
+  expected_reward = LevelService.get_expected_reward(level)
+  if expected_reward:
+    return jsonify(expected_reward)
+  return jsonify({'error': 'Level not found'}), 404
