5a7ba9742c1a66c235c3f1aeb4172c5fe11db864
LucMagg <luc.maggiotto@gmail.com>
2025-11-15 07:11:35 +0100

    update language model
    -> adaptation du modèle language aux yaml déjà présents sur le fndwikiupdater

--- app/models/language.py
+++ app/models/language.py
@@ -3,83 +3,27 @@ from typing import Dict, Optional, Union, List
 from ..utils.strUtils import str_to_slug
 
 
-class Choice:
-  def __init__(self, name: str, value: str):
-    self.name = name
-    self.value = value
-
-  @classmethod
-  def from_dict(cls, data: Dict):
-    return cls(
-      name = data.get('name'),
-      value = data.get('value')
-    )
-  
-  def to_dict(self) -> Dict:
-    return {
-      'name': self.name,
-      'value': self.value
-    }
-
-
-class Option:
-  def __init__(self, name: str, type: int, description: str, required: bool, choices: Union[List[Choice], List] = None):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.required = required
-    self.choices = choices
-
-  @classmethod
-  def from_dict(cls, data: Dict):
-    return cls(
-      name = data.get('name'),
-      type = data.get('type'),
-      description = data.get('description'),
-      required = data.get('required'),
-      choices = [Choice.from_dict(choice_data) for choice_data in data.get('choices', []) if isinstance(choice_data, dict)]
-    )
-  
-  def to_dict(self) -> Dict:
-    return {
-      'name': self.name,
-      'type': self.type,
-      'description': self.description,
-      'required': self.required,
-      'choices': [choice.to_dict() for choice in self.choices] if self.choices else []
-    }
-  
-
 class Language:
-  def __init__(self, name: str, type: int, description: str, to_update: bool, setup_type: Optional[str] = None, _id: Optional[str] = None, options: Union[List[Option], List] = None):
+  def __init__(self, name: str, code: str, translations: List, _id: Optional[str] = None):
     self._id = ObjectId(_id) if _id else None
     self.name = name
-    self.type = type
-    self.setup_type = setup_type
-    self.to_update = to_update
-    self.description = description
-    self.options = options
+    self.code = code
+    self.translations = translations
 
   @classmethod
   def from_dict(cls, data: Dict):
     return cls(
       _id = str(data.get('_id')) if data.get('_id') else None,
-      name = data.get('name'),
-      type = data.get('type'),
-      setup_type = data.get('setup_type'),
-      to_update = data.get('to_update'),
-      description = data.get('description'),
-      options = [Option.from_dict(option_data) for option_data in data.get('options', []) if isinstance(option_data, dict)]
+      name = data.get('Name'),
+      code = data.get('Code'),
+      translations = data.get('Translations'),
     )
 
   def to_dict(self) -> Dict:
     language = {
       'name': self.name,
-      'type': self.type,
-      'setup_type': self.setup_type,
-      'to_update': self.to_update,
-      'description': self.description,
-      'options': [option.to_dict() for option in self.options] if self.options else []
+      'code': self.code,
+      'translations': self.translations,
     }
     if self._id:
       language['_id'] = str(self._id)
